9b1d59e31fe9c3c4aa093b571cb94aa31c52a702
zygmuntdzwigon <zygmuntdxwigon@gmail.com>
2020-04-11 18:49:21 +0200

    message struct update

--- UE/Application/Ports/DbPort.cpp
+++ UE/Application/Ports/DbPort.cpp
@@ -27,14 +27,20 @@ void DbPort::stop()
 
 int DbPort::saveSentSms(const common::PhoneNumber& to, const std::string& message)
 {
-    DbMessage msg{ -1, message, phoneNumber.value, to.value };
+    DbMessage msg{ -1, message, phoneNumber.value, to.value, sent };
     return db->insert(msg);
 }
 
 int DbPort::saveReceivedSms(const common::PhoneNumber& from, const std::string& message)
 {
-    DbMessage msg{ -1, message, from.value, phoneNumber.value };
+    DbMessage msg{ -1, message, from.value, phoneNumber.value, not_read };
     return db->insert(msg);
+}
+
+std::vector<DbMessage> DbPort::getAllMessages()
+{
+    std::vector<DbMessage> messages = db->get_all<DbMessage>();
+    return messages;
 }
 
 }
--- UE/Application/Ports/IDbPort.hpp
+++ UE/Application/Ports/IDbPort.hpp
@@ -2,15 +2,22 @@
 
 #include "Messages/PhoneNumber.hpp"
 #include <string>
+#include <vector>
 
 namespace ue
 {
+enum MessageStatuses{
+    not_read,
+    read,
+    sent
+};
 
 struct DbMessage {
     int id;
     std::string text;
     int fromNumber;
     int toNumber;
+    int status;
 };
 
 class IDbPort
@@ -20,6 +27,7 @@ public:
 
     virtual int saveSentSms(const common::PhoneNumber& to, const std::string& message) = 0;
     virtual int saveReceivedSms(const common::PhoneNumber& from, const std::string& message) = 0;
+    virtual std::vector<DbMessage> getAllMessages() = 0;
 };
 
 }
