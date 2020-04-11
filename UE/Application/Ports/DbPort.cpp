21a7ac50b7501d3927f68494c7375cd4b188ccb1
mdarocha <git@mdarocha.pl>
2020-04-11 16:47:05 +0200

    Init database

--- UE/Application/Ports/DbPort.cpp
+++ UE/Application/Ports/DbPort.cpp
@@ -1,5 +1,7 @@
 #include "DbPort.hpp"
 #include <sstream>
+#include "sqlite_orm.h"
+using namespace sqlite_orm;
 
 namespace ue
 {
@@ -9,11 +11,13 @@ DbPort::DbPort(common::PhoneNumber number) : phoneNumber(number)
     std::ostringstream os;
     os << "db-" << (int) number.value << ".db";
     databasePath = os.str();
+
+    db = std::make_unique<Storage>(initStorage(databasePath));
 }
 
 void DbPort::start()
 {
-
+    db->sync_schema(true);
 }
 
 void DbPort::stop()
--- UE/Application/Ports/DbPort.hpp
+++ UE/Application/Ports/DbPort.hpp
@@ -1,18 +1,31 @@
 #pragma once
 
 #include<string>
+#include "sqlite_orm.h"
 #include "Messages/PhoneNumber.hpp"
 #include "IDbPort.hpp"
 
 namespace ue
 {
 
-class DbMessage {
-    public:
-        std::string text;
-        int fromNumber;
-        int toNumber;
+struct DbMessage {
+    int id;
+    std::string text;
+    int fromNumber;
+    int toNumber;
 };
+
+inline auto initStorage(const std::string& path) {
+    using namespace sqlite_orm;
+    return make_storage(path,
+            make_table("Messages",
+                make_column("Id", &DbMessage::id,
+                    primary_key(), autoincrement()),
+                make_column("Text", &DbMessage::text),
+                make_column("FromNumber", &DbMessage::fromNumber),
+                make_column("ToNumber", &DbMessage::toNumber)));
+}
+using Storage = decltype(initStorage(""));
 
 class DbPort : public IDbPort
 {
@@ -26,6 +39,7 @@ class DbPort : public IDbPort
     private:
         common::PhoneNumber phoneNumber;
         std::string databasePath;
+        std::unique_ptr<Storage> db;
 };
 
 }
