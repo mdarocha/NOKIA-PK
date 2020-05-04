a1e847dd77f0c1729b99c21a550bed866a6e0132
Karol <karolkarol190@gmail.com>
2020-05-04 12:40:26 +0200

    Fix Fix Errors

--- UE/Application/States/TalkingState.hpp
+++ UE/Application/States/TalkingState.hpp
@@ -9,7 +9,7 @@ class TalkingState : public BaseState
 {
 public:
     TalkingState(Context& context, common::PhoneNumber number);
-    void handleSendCallAccept(common::PhoneNumber recipient) final;
+    void handleUnknownRecipientAfterCallAccepted() final;
     common::PhoneNumber recipientNumber;
 };
 
